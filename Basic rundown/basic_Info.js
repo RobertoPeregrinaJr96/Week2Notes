fa4f5c3d0755c98da4663f4a857afb2948feeb03
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-26 17:41:52 -0800

    update

--- Basic rundown/basic_Info.js
+++ Basic rundown/basic_Info.js
@@ -15,7 +15,7 @@ console.log("Arrow function Syntax")
 
 // ----> Arrow function <----
 
-// we can ommite most of the fluff ; first we declare a variable using the const,let(you can use var or no keyword at all but thats a can of worms left alone)
+// we can omit most of the fluff ; first we declare a variable using the const,let(you can use var or no keyword at all but thats a can of worms left alone)
 const sum = (arr) => {
     let sum = 0;
     for (let element of arr) {
@@ -25,3 +25,41 @@ const sum = (arr) => {
 }
 
 console.log(sum([1, 2, 3, 4, 5]))
+
+console.log("--------------------------------------------------")
+
+console.log("Iterating through Arrays")
+/*
+for of helps shorthand a for loop
+-for of loop targets the elements in array and iterate through every element
+*/
+let arr = [1, 2, 3, 4, 5]
+
+for (let element of arr) {
+    console.log(element)
+}
+// when using a for in loop on a array the element isn't returned but the indices is
+for (let element in arr) {
+    console.log(element)
+}
+console.log("--------------------------------------------------")
+
+console.log("Rest Syntax")
+
+const allTheArg = (...args) => {
+    for(let arg of args){
+        console.log(arg)
+    }
+}
+
+allTheArg(1,2,3,4,5,6,76,435,3233)
+
+console.log("Spread Operator")
+
+const spreadItAll = (arr) => {
+    let sum = 0
+    sum += [...arr]
+    return sum
+}
+
+console.log(spreadItAll([1,2,3,4,5]))
--- Basic rundown/objects.js
+++ Basic rundown/objects.js
@@ -11,54 +11,70 @@ Objects are data structures
 
 let obj = { a: 1, b: 2, c: 3, d: 4 }//  <-- the keys are the letters and the value are the numbers
 console.table(obj)
-for (let keys in obj) {
-    // by using the for in loop we can
-    console.log(keys)
-    // similar to accessing elements in a array; we can
-    let value = obj[keys]
-    console.log(value)
-}
+//there are two different ways to access a key in a object
+// -- first is what we call *Dot Notation* which we the the object follow with the key and a period separating them
+console.log(obj.a)
+
+// the other way is called *Bracket Notation* which we use when the key is a variable,assigning a string as key and when a number is the first character  of the key
+
+obj["this is a string"] = "this is the value"
+console.log(obj["this is a string"])
+console.table(obj)
 
 
-
-
-
-
-
-
-
-
-
+obj["2abc"] = 10
+console.log(obj["2abc"])
+console.table(obj)
 
 console.log("--------------------------------------------------")
+
+let obj1 = { a: 1, b: 2, c: 3, d: 4 }
+
+for (let keys in obj1) {
+    // by using the for in loop we can iterate through the keys of the object
+    console.log(keys)
+    // similar to accessing elements in a array; we can access the values of keys by our specified key in [] on the right of the object
+    let value = obj1[keys]
+    console.log(value)
+}
+console.log("--------------------------------------------------")
+// obj1 = { a: 1, b: 2, c: 3, d: 4 }
 console.log("Iterating through Objects")
 /*
-when iterating through a object we have to becareful as objects are unordered in theyre indices
+when iterating through a object we have to careful as objects are unordered in theyre indices
 -for in loop targets the keys in the and iterate through every key
 */
 
-for (let keys in obj) {
+for (let keys in obj1) {
     console.log(keys)
     // we can assign values to variable the same way we attach array[index]
-    let value = obj[keys]
+    let value = obj1[keys]
     console.log(value)
 }
 // try not to iterate a object with a for of loop
 // for(let keys of obj){      // TypeError: obj is not iterable
 //     console.log(keys)
 // }
-console.log("--------------------------------------------------")
-console.log("Iterating through Arrays")
-/*
-for of helps shorthand a for loop
--for of loop targets the elements in array and iterate through every element
-*/
-let arr = [1, 2, 3, 4, 5]
 
-for (let element of arr) {
-    console.log(element)
+console.log("--------------------------------------------------")
+
+console.log("Common Object Methods")
+// obj1 = { a: 1, b: 2, c: 3, d: 4 }
+console.log("Object.keys()")
+// declare the
+const allTheKeys = (obj1) => {
+    // by using the Object.keys() method we get back a array of the keys of the object
+    const keys = Object.keys(obj1) // ["a","b","c","d"]
+    return keys
 }
-// when using a for in loop on a array the element isnt returned but the indices is
-for (let element in arr) {
-    console.log(element)
+console.log(allTheKeys(obj1))// ["a","b","c","d"]
+
+console.log("Object.values()")
+const allTheValue = (obj1) => {
+    // Object.values works the same as Object.keys() as it returns a array containing all of the values within the object
+    const value = Object.values(obj1) // [1,2,3,4]
+    return value
 }
+console.log(allTheValue(obj1)) // [1,2,3,4]
+
+console.log("--------------------------------------------------")
